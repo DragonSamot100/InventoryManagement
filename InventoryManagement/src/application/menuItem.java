6897f175aff05bff28bc80d4c1aab01dfad0cd76
Perfetta <perfetta@go.stockton.edu>
2020-04-23 15:58:46 -0400

    fix plz

--- InventoryManagement/src/application/menuItem.java
+++ InventoryManagement/src/application/menuItem.java
@@ -1,9 +1,18 @@
 package application;
-//test push
+
+import java.util.ArrayList;
+
 public class menuItem 
 {
+	int menuItemID;
 	public menuItem()
+	{
+		menuItemID++;
+	}
+	
+	public menuItem(String name, ArrayList<inventoryItem> ingredients)
 	{
 		
 	}
+	
 }
