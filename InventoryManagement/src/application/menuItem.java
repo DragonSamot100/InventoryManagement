fbb3dfe3cfc19cd1536553c6a191c59c06df6e44
Tom Olandt <to6733@gmail.com>
2020-04-23 15:57:07 -0400

    New menuItem

--- InventoryManagement/src/application/menuItem.java
+++ InventoryManagement/src/application/menuItem.java
@@ -1,9 +1,21 @@
 package application;
-//test push
+import java.util.ArrayList;
+
 public class menuItem 
 {
-	public menuItem()
-	{
-		
+	private ArrayList<inventoryItem> itemList;
+	private int menuID;
+	private static int menuIDcount = 0;
+	private String name;
+	
+	
+	public menuItem(String itemName, ArrayList<inventoryItem> list){
+		name = itemName;
+		menuID = menuIDcount ++;
+		itemList = list;
+	}
+	
+	public int getID() {
+		return menuID;
 	}
 }
