f12ba73994e71dd8ac9818863b273016a48a56df
Tom Olandt <to6733@gmail.com>
2020-04-27 19:54:37 -0400

    SCVReader fix - Scans a file, not just the string

--- InventoryManagement/src/application/CSVReader.java
+++ InventoryManagement/src/application/CSVReader.java
@@ -10,12 +10,15 @@ public class CSVReader {
 	
 	public static ObservableList<inventoryItem> read(String filePath){
 		ArrayList<inventoryItem> inventory = new ArrayList();
-		try (Scanner in = new Scanner(filePath)){
+		try (Scanner in = new Scanner(new File(filePath))){
 			in.nextLine(); //Something with date here
 			String line;
 			while(in.hasNextLine()) {
 				line = in.nextLine();
 				String[] split = line.split(",");
+				for (int i = 0; i < 6; i ++) {
+					split[i] = split[i].trim();
+				}
 				inventoryItem item = new inventoryItem(split[1], Integer.parseInt(split[0]), Integer.parseInt(split[2]), split[3], split[4], split[5]);
 				inventory.add(item);
 			}
@@ -23,6 +26,27 @@ public class CSVReader {
 			e.printStackTrace();
 		}
 		return FXCollections.observableArrayList(inventory);
+	}
+	
+	public static ArrayList<inventoryItem> readAsArray(String filePath){
+		ArrayList<inventoryItem> inventory = new ArrayList();
+		try (Scanner in = new Scanner(new File(filePath))){
+			in.nextLine(); //Something with date here
+			String line;
+			while(in.hasNextLine()) {
+				line = in.nextLine();
+				String[] split = line.split(",");
+				for (int i = 0; i < 6; i ++) {
+					split[i] = split[i].trim();
+					System.out.println(split[i]);
+				}
+				inventoryItem item = new inventoryItem(split[1], Integer.parseInt(split[0]), Integer.parseInt(split[2]), split[3], split[4], split[5]);
+				inventory.add(item);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return inventory;
 	}
 	
 	
